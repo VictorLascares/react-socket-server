7242bd1c547b1d01431ec2ffb9ef69f922cec4e9
Victor Lascares <vmlascares@gmail.com>
2022-01-17 00:19:24 -0600

    Primer Commit

---
+++ models/server.js
@@ -0,0 +1,44 @@
+// Servidor de express
+const express = require("express");
+const { createServer } = require("http");
+const socketio = require("socket.io");
+const path = require('path');
+const Sockets = require("./sockets");
+
+class Server {
+    constructor() {
+        this.app = express()
+        this.port = process.env.PORT
+
+        // HTTP server
+        this.server = createServer(this.app)
+
+        // Configuraciones de sockets
+        this.io = socketio( this.server , {/* Configuraciones */})
+    }
+    middlewares() {
+        // Desplegar el directorio publico
+        this.app.use( express.static( path.resolve(__dirname, '../public' ) ) )
+    }
+
+    configurarSockets() {
+        new Sockets(this.io)
+    }
+
+
+    execute() {
+        // Inicializar middlewares
+        this.middlewares();
+
+        // Inicializar sockets
+        this.configurarSockets()
+
+        // Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo en puerto: ', this.port);
+        });
+    }
+}
+
+
+module.exports = Server
