ce9dbf0fa2040f27935d63758dc091ff8f5b4cfe
Victor Lascares <vmlascares@gmail.com>
2022-01-17 00:51:06 -0600

    Habilitando CORS

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { createServer } = require("http");
 const socketio = require("socket.io");
 const path = require('path');
+const cors = require('cors');
 const Sockets = require("./sockets");
 
 class Server {
@@ -19,6 +20,9 @@ class Server {
     middlewares() {
         // Desplegar el directorio publico
         this.app.use( express.static( path.resolve(__dirname, '../public' ) ) )
+        
+        // CORS
+        this.app.use( cors() )
     }
 
     configurarSockets() {
